a828e20b702cd060592f2be087968243fac98009
veronisl <islamova.va@edu.spbstu.ru>
2026-01-29 12:24:55 +0300

    Лабораторная работа 1

---
+++ task_3.py
@@ -0,0 +1,8 @@
+list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
+
+people_ = len(list_players) // 2
+first_players = list_players[:people_]
+last_players = list_players[people_:]
+
+print(first_players)
+print(last_players)
